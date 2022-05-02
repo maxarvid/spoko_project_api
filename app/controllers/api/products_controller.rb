07f0545b69df22e2060f522f565084e0c37a4279
gsim1903 <gavin@internetbusker.co>
2022-05-02 09:13:06 +0200

    adds show action for specific product test

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,17 @@
 class Api::ProductsController < ApplicationController
+  
   def index
     products = Product.all 
     render json: { products: products }
   end
+
+  def show
+    product = Product.find(params['id'])
+    render json: { product: product }
+
+  end
+
+
+
+
 end
