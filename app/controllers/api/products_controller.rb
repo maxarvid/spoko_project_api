52691b0bdb29a14fa570b987bd8c07333311c1f3
maxarvid <max.arvid.anderson@gmail.com>
2022-05-03 09:48:34 -0400

    renames index action file, adds sad path to show action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,15 +1,13 @@
 class Api::ProductsController < ApplicationController
-  
   def index
-    products = Product.all 
-    render json: { products: products }
+    products = Product.all
+    render json: { products: }
   end
 
   def show
     product = Product.find(params['id'])
     render json: { product: product }
-
+  rescue StandardError
+    render json: { error: 'No such item exists' }, status: 422
   end
-
-
 end
