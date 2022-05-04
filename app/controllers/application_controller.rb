cd9697f0d3a500b601547641308c786467960abf
maxarvid <max.arvid.anderson@gmail.com>
2022-05-04 13:35:59 -0400

    small refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include Pundit::Authorization
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    binding.pry
+  end
 end
--- spec/requests/api/orders/create_spec.rb
+++ spec/requests/api/orders/create_spec.rb
@@ -1,32 +1,51 @@
 RSpec.describe 'Create an order with POST /api/orders', type: :request do
   let(:user) { create(:user) }
+  let(:editor) { create(:user, role: 1) }
   let(:product) { create(:product) }
   subject { response }
 
-  before do
-    post '/api/orders', params: {
-      order: {
-        product_id: product.id, user_id: user.id
+  describe 'successfully' do
+    before do
+      post '/api/orders', params: {
+        order: {
+          product_id: product.id, user_id: user.id
+        }
       }
-    }
-    @order = Order.last
+      @order = Order.last
+    end
+
+    it { is_expected.to have_http_status 201 }
+
+    it 'is expected to create an instance of Order' do
+      expect(@order).to_not eq nil
+    end
+
+    it 'is expected to associate an Order with user' do
+      expect(@order.user).to eq user
+    end
+
+    it 'is expected to create an order_item' do
+      expect(@order.items).to_not eq nil
+    end
+
+    it 'is expected to assocaite the product with an order ' do
+      expect(@order.products.first).to eq product
+    end
   end
 
-  it { is_expected.to have_http_status 201 }
+  describe 'unsuccessfully' do
+    before do
+      post '/api/orders', params: {
+        order: {
+          product_id: product.id, user_id: editor.id
+        }
+      }
+    end
 
-  it 'is expected to create an instance of Order' do
-    expect(@order).to_not eq nil
-  end
+    it { binding.pry; is_expected.to have_http_status 401 }
 
-  it 'is expected to associate an Order with user' do
-    expect(@order.user).to eq user
-  end
-
-  it 'is expected to create an order_item' do
-    expect(@order.items).to_not eq nil
-  end
-
-  it 'is expected to assocaite the product with an order ' do
-    expect(@order.products.first).to eq product
+    it 'is expected to respond with an error message' do
+      expect(response_json['error']).to eq 'You are not permitted to perform that action'
+    end
   end
 end
