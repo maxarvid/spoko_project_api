d5256e506d6e2a70c73516da378e4fb54e6e7b59
gsim1903 <gavin@internetbusker.co>
2022-05-14 11:32:02 +0200

    refactor tests

--- spec/requests/api/orders/update_spec.rb
+++ spec/requests/api/orders/update_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe 'Update an order with a PUT /api/orders/:id', type: :request do
   let(:product_3) { create(:product, name: 'Scarf',price: 300) }
   let(:order) { create(:order) }
   let!(:order_item_1) { create(:order_item, order:, product: product_2) }
-  let!(:order_item_2) { create(:order_item, order:, product: product_3) }
+  let!(:order_item_2) { create(:order_item, order:, product: product_1) }
 
   subject { response }
 
   describe 'succesful order' do
     before do
-      put "/api/orders/#{order.id}", params: { product_id: product_1.id }
+      put "/api/orders/#{order.id}", params: { product_id: product_3.id }
       @order = Order.last
     end
 
     it { is_expected.to have_http_status :ok }
 
     it 'response with an order confirmation' do
-      expect(response_json['message']).to eq 'Football added'
+      expect(response_json['message']).to eq 'Scarf added'
     end
 
     it 'is expected to contain 3 items' do
@@ -30,7 +30,6 @@ RSpec.describe 'Update an order with a PUT /api/orders/:id', type: :request do
     end
 
      it 'is to generate to generate a value of 600 in order' do
-      binding.pry
        expect(@order.order_value).to eq 600
      end
     
